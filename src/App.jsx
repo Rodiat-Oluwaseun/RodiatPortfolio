d841b3a9c43eb37b7bd94eb487d2491ebdfd4b5c
rodiat-oluwaseun <yemirolayemi@gmail.com>
2024-01-09 14:17:04 +0000

    project created with folder

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ContactPage from './components/ContactPage';
+import Project from './components/Project';
+
+
+function App() {
+  return <div>
+    <ContactPage />
+    <Project />
+
+  </div>
+}
+
+export default App;
